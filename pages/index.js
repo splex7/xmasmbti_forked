c791f31b0d6a2c9b2509294bd3a3a81e5aad9541
Hayden <devyeony@gmail.com>
2021-12-19 12:55:36 +0900

    파비콘 수정 및 썸네일 추가

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,13 @@ export default function Home() {
             <Head>
                 <title>크리스마스 트리 MBTI</title>
                 <link rel="icon" href="/favicon.ico"/>
+                <meta property="og:url" content="https://xmas-tree-mbti.netlify.app/" />
+                <meta property="og:title" content="크리스마스 트리 MBTI" />
+                <meta
+                    property="og:description"
+                    content="캐롤송으로 알아보는 크리스마스 트리 MBTI"
+                />
+                <meta property="og:image" content="thumbnail.png" />
             </Head>
 
             <main>
